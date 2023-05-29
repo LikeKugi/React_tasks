8968fb9b9f0917d2e65050151da554a4b1d9ef75
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-05-29 21:54:54 +0300

    init: start styles in react

---
+++ modern_react/styles-project/src/components/Tasks/TaskInput/TaskInput.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+
+import Button from "../../UI/Button/Button";
+import "./TaskInput.css";
+
+const TaskInput = (props) => {
+    const [inputText, setInputText] = useState("");
+    const [isInputValid, setInputValid] = useState(true);
+
+    const taskInputChangeHandler = (event) => {
+        if (event.target.value.trim() > 0) {
+            setInputValid(true);
+        }
+        setInputText(event.target.value);
+    };
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        if (inputText.trim().length === 0) {
+            setInputValid(false);
+            return;
+        }
+        props.onAddTask(inputText);
+    };
+
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className="form-control">
+                <label style={{color: !isInputValid ? 'red' : 'black'}}>Задачи</label>
+                <input style={{borderColor: !isInputValid ? 'red' : 'black', backgroundColor: !isInputValid ? 'salmon' : 'white'}} type="text" onChange={taskInputChangeHandler} />
+            </div>
+            <Button type="submit">Добавить Задачу</Button>
+        </form>
+    );
+};
+
+export default TaskInput;
