f1d97c764a693e8662af2afda9972e283398df77
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2024-01-28 18:31:48 +0300

    init: initial commit

---
+++ makeweb-react-ant/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from 'antd';
+
+function App() {
+
+  return (
+    <>
+    <h1>test</h1>
+      <div>
+        <Button type="primary">Primary button</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
